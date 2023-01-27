569b0505ce894aaad61a5d29d94ad0661930a012
Tommy Bryson <118629810+TommyBryson@users.noreply.github.com>
2023-01-27 14:13:53 +0300

    Update apple.js

--- apple.js
+++ apple.js
@@ -1,13 +1,13 @@
-const outputNode = document.querySelector('.sl7-input');
-const btn1 = document.querySelector('.btn1');
-const btn2 = document.querySelector('.btn2');
-const btn3 = document.querySelector('.btn3');
-const btn4 = document.querySelector('.btn4');
-const btn5 = document.querySelector('.btn5');
-const btn6 = document.querySelector('.btn6');
-const btn7 = document.querySelector('.btn7');
-const btn8 = document.querySelector('.btn8');
-const btn9 = document.querySelector('.btn9');
+const outputNode = document.querySelector('.sl7-input')
+const btn1 = document.querySelector('.btn1')
+const btn2 = document.querySelector('.btn2')
+const btn3 = document.querySelector('.btn3')
+const btn4 = document.querySelector('.btn4')
+const btn5 = document.querySelector('.btn5')
+const btn6 = document.querySelector('.btn6')
+const btn7 = document.querySelector('.btn7')
+const btn8 = document.querySelector('.btn8')
+const btn9 = document.querySelector('.btn9')
 const search = document.querySelector('.sl7-search')
 
 btn1.addEventListener('click', ( ) => {
